8baa1030e338db205a95d0ea436491b614383abe
Leo Fang <leof@nvidia.com>
2024-09-03 08:08:55 +0000

    expose options to top level namespace

--- cuda_py/cuda/py/__init__.py
+++ cuda_py/cuda/py/__init__.py
@@ -1,5 +1,6 @@
 from cuda.py._compiler import Compiler
 from cuda.py._device import Device
+from cuda.py._event import EventOptions
 from cuda.py._launcher import LaunchConfig, launch
-from cuda.py._stream import Stream
+from cuda.py._stream import Stream, StreamOptions
 from cuda.py._version import __version__
