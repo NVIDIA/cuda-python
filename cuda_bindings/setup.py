f23576b69d22727a5fc8e91293f9eccb3fb6a6bc
Vladislav Zhurba <vzhurba@nvidia.com>
2024-11-06 14:11:53 -0800

    Use pyproject for auto discovery of packages and data

--- cuda_bindings/setup.py
+++ cuda_bindings/setup.py
@@ -258,11 +258,6 @@ cmdclass = versioneer.get_cmdclass(cmdclass)
 setup(
     version=versioneer.get_version(),
     ext_modules=do_cythonize(extensions),
-    packages=find_packages(include=["cuda", "cuda.*", "cuda.bindings", "cuda.bindings._bindings", "cuda.bindings._lib", "cuda.bindings._lib.cyruntime"]),
-    package_data=dict.fromkeys(
-        find_packages(include=["cuda", "cuda.*", "cuda.bindings", "cuda.bindings._bindings", "cuda.bindings._lib", "cuda.bindings._lib.cyruntime"]),
-        ["*.pxd", "*.pyx", "*.py", "*.h", "*.cpp"],
-    ),
     cmdclass=cmdclass,
     zip_safe=False,
 )
