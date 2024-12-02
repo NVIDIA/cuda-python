c8a8dcb0a682ab754e1d036c68dc312a0b97608d
ksimpson <ksimpson@nvidia.com>
2024-12-02 13:24:50 -0800

    save

--- cuda_core/cuda/core/experimental/_linker.py
+++ cuda_core/cuda/core/experimental/_linker.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
+
 import weakref
 from dataclasses import dataclass
 from typing import List, Optional
