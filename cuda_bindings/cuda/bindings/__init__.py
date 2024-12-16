43800c6d8c5e75731828b48a24a56b6a79ad70e8
Vladislav Zhurba <vzhurba@nvidia.com>
2024-12-16 13:18:51 -0800

    Add copyright for consistency
    (cherry picked from commit bfa2bc6df45216f41ae97d91612db54418c6edba)

--- cuda_bindings/cuda/bindings/__init__.py
+++ cuda_bindings/cuda/bindings/__init__.py
@@ -1,1 +1,9 @@
+# Copyright 2024 NVIDIA Corporation.  All rights reserved.
+#
+# Please refer to the NVIDIA end user license agreement (EULA) associated
+# with this source code for terms and conditions that govern your use of
+# this software. Any use, reproduction, disclosure, or distribution of
+# this software and related documentation outside the terms of the EULA
+# is strictly prohibited.
+
 from cuda.bindings._version import __version__
