2ebacd6c0705aa6a660e4b71f33f013157ebac68
Leo Fang <leof@nvidia.com>
2025-01-02 14:15:08 -0500

    Set up a preliminary doc build/publish pipeline  (#325)
    * add a doc build workflow
    
    * keep build artifact for later use
    
    * skip test stage for now
    
    * fix typo
    
    * reduce build matrix; ensure doc stage is triggered; simplify cuda-core install
    
    * switch runner & install CTK for doc build
    
    * fix latest-only logic
    
    * switch to T4 runner due to long latency
    
    * skip concurrency setup to unblock myself
    
    * change artifact layout to match gh-pages; add a deploy job
    
    * add manual deployment borrowed from the array-api repo
    
    * reduce script diff
    
    * nit: update workflow comments
    
    * add a conda env check step
    
    * bring back full CI + run nvidia-smi in doc CI
    
    * try github-pages-deploy-action
    
    * remove dry-run tag
    
    * update concurrency group name
    
    * incorporate the ci-gh workflow into build-and-test + ensure all workflows have a name
    
    ---------
    
    Co-authored-by: Keenan Simpson <ksimpson@nvidia.com>

--- cuda_bindings/docs/build_docs.sh
+++ cuda_bindings/docs/build_docs.sh
@@ -2,6 +2,15 @@
 
 set -ex
 
+if [[ "$#" == "0" ]]; then
+    LATEST_ONLY="0"
+elif [[ "$#" == "1" && "$1" == "latest-only" ]]; then
+    LATEST_ONLY="1"
+else
+    echo "usage: ./build_docs.sh [latest-only]"
+    exit 1
+fi
+
 # SPHINX_CUDA_BINDINGS_VER is used to create a subdir under build/html
 # (the Makefile file for sphinx-build also honors it if defined).
 # If there's a post release (ex: .post1) we don't want it to show up in the
@@ -28,7 +37,11 @@ cp ./versions.json build/html
 cp source/_templates/main.html build/html/index.html
 
 # ensure that the latest docs is the one we built
-cp -r build/html/${SPHINX_CUDA_BINDINGS_VER} build/html/latest
+if [[ $LATEST_ONLY == "0" ]]; then
+    cp -r build/html/${SPHINX_CUDA_BINDINGS_VER} build/html/latest
+else
+    mv build/html/${SPHINX_CUDA_BINDINGS_VER} build/html/latest
+fi
 
 # ensure that the Sphinx reference uses the latest docs
 cp build/html/latest/objects.inv build/html
