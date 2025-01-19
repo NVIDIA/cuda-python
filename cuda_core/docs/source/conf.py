963261f68751e02175d660bb7ceb981c872424e0
Leo Fang <leof@nvidia.com>
2025-01-18 23:00:01 -0500

    propagate the banner to cuda-core

--- cuda_core/docs/source/conf.py
+++ cuda_core/docs/source/conf.py
@@ -75,6 +75,12 @@ html_theme_options = {
     #    "navbar-icon-links",
     # ],
 }
+if os.environ.get("CI"):
+    PR_NUMBER = f"{os.environ['PR_NUMBER']}"
+    PR_TEXT = f'<a href="https://github.com/NVIDIA/cuda-python/pull/{PR_NUMBER}">PR {PR_NUMBER}</a>'
+    html_theme_options["announcement"] = (
+        f"<em>Warning</em>: This documentation is only a preview for {PR_TEXT}!"
+    )
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
