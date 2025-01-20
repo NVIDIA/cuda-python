5d143cdeda021804147732e9f342f0f10c245af0
Leo Fang <leof@nvidia.com>
2025-01-20 19:55:46 +0000

    fix for local doc builds

--- cuda_bindings/docs/source/conf.py
+++ cuda_bindings/docs/source/conf.py
@@ -71,11 +71,11 @@ html_theme_options = {
     # ],
 }
 if os.environ.get("CI"):
-    if int(os.environ.get("BUILD_PREVIEW")):
+    if int(os.environ.get("BUILD_PREVIEW", 0)):
         PR_NUMBER = f"{os.environ['PR_NUMBER']}"
         PR_TEXT = f'<a href="https://github.com/NVIDIA/cuda-python/pull/{PR_NUMBER}">PR {PR_NUMBER}</a>'
         html_theme_options["announcement"] = f"<em>Warning</em>: This documentation is only a preview for {PR_TEXT}!"
-    elif int(os.environ.get("BUILD_LATEST")):
+    elif int(os.environ.get("BUILD_LATEST", 0)):
         html_theme_options["announcement"] = (
             "<em>Warning</em>: This documentation is built from the development branch!"
         )
