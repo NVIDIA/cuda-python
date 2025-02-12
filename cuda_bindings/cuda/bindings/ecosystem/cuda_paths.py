586d2092f35708a16ad5801e57cf0f5b0050730e
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-02-12 14:02:06 -0800

    ruff auto-fixes (NO manual changes)

--- cuda_bindings/cuda/bindings/ecosystem/cuda_paths.py
+++ cuda_bindings/cuda/bindings/ecosystem/cuda_paths.py
@@ -1,17 +1,15 @@
 # Forked from:
 # https://github.com/NVIDIA/numba-cuda/blob/bf487d78a40eea87f009d636882a5000a7524c95/numba_cuda/numba/cuda/cuda_paths.py
 
-import sys
-import re
 import os
+import sys
 from collections import namedtuple
-import platform
 
-from findlib import find_file, find_lib
+from findlib import find_lib
 
-IS_WIN32 = sys.platform.startswith('win32')
+IS_WIN32 = sys.platform.startswith("win32")
 
-_env_path_tuple = namedtuple('_env_path_tuple', ['by', 'info'])
+_env_path_tuple = namedtuple("_env_path_tuple", ["by", "info"])
 
 
 def _find_valid_path(options):
@@ -23,22 +21,22 @@ def _find_valid_path(options):
         if data is not None:
             return by, data
     else:
-        return '<unknown>', None
+        return "<unknown>", None
 
 
 def _nvvm_lib_dir():
     if IS_WIN32:
-        return 'nvvm', 'bin'
+        return "nvvm", "bin"
     else:
-        return 'nvvm', 'lib64'
+        return "nvvm", "lib64"
 
 
 def _get_nvvm_path_decision():
     options = [
-        ('Conda environment', get_conda_ctk()),
-        ('Conda environment (NVIDIA package)', get_nvidia_nvvm_ctk()),
-        ('CUDA_HOME', get_cuda_home(*_nvvm_lib_dir())),
-        ('System', get_system_ctk(*_nvvm_lib_dir())),
+        ("Conda environment", get_conda_ctk()),
+        ("Conda environment (NVIDIA package)", get_nvidia_nvvm_ctk()),
+        ("CUDA_HOME", get_cuda_home(*_nvvm_lib_dir())),
+        ("System", get_system_ctk(*_nvvm_lib_dir())),
     ]
     by, path = _find_valid_path(options)
     return by, path
@@ -46,31 +44,29 @@ def _get_nvvm_path_decision():
 
 def _cudalib_path():
     if IS_WIN32:
-        return 'bin'
+        return "bin"
     else:
-        return 'lib64'
+        return "lib64"
 
 
 def get_system_ctk(*subdirs):
-    """Return path to system-wide cudatoolkit; or, None if it doesn't exist.
-    """
+    """Return path to system-wide cudatoolkit; or, None if it doesn't exist."""
     # Linux?
-    if sys.platform.startswith('linux'):
+    if sys.platform.startswith("linux"):
         # Is cuda alias to /usr/local/cuda?
         # We are intentionally not getting versioned cuda installation.
-        base = '/usr/local/cuda'
+        base = "/usr/local/cuda"
         if os.path.exists(base):
             return os.path.join(base, *subdirs)
 
 
 def get_conda_ctk():
-    """Return path to directory containing the shared libraries of cudatoolkit.
-    """
-    is_conda_env = os.path.exists(os.path.join(sys.prefix, 'conda-meta'))
+    """Return path to directory containing the shared libraries of cudatoolkit."""
+    is_conda_env = os.path.exists(os.path.join(sys.prefix, "conda-meta"))
     if not is_conda_env:
         return
     # Assume the existence of NVVM to imply cudatoolkit installed
-    paths = find_lib('nvvm')
+    paths = find_lib("nvvm")
     if not paths:
         return
     # Use the directory name of the max path
@@ -78,9 +74,8 @@ def get_conda_ctk():
 
 
 def get_nvidia_nvvm_ctk():
-    """Return path to directory containing the NVVM shared library.
-    """
-    is_conda_env = os.path.exists(os.path.join(sys.prefix, 'conda-meta'))
+    """Return path to directory containing the NVVM shared library."""
+    is_conda_env = os.path.exists(os.path.join(sys.prefix, "conda-meta"))
     if not is_conda_env:
         return
 
@@ -88,16 +83,16 @@ def get_nvidia_nvvm_ctk():
     # conda package is installed.
 
     # First, try the location used on Linux and the Windows 11.x packages
-    libdir = os.path.join(sys.prefix, 'nvvm', _cudalib_path())
+    libdir = os.path.join(sys.prefix, "nvvm", _cudalib_path())
     if not os.path.exists(libdir) or not os.path.isdir(libdir):
         # If that fails, try the location used for Windows 12.x packages
-        libdir = os.path.join(sys.prefix, 'Library', 'nvvm', _cudalib_path())
+        libdir = os.path.join(sys.prefix, "Library", "nvvm", _cudalib_path())
         if not os.path.exists(libdir) or not os.path.isdir(libdir):
             # If that doesn't exist either, assume we don't have the NVIDIA
             # conda package
             return
 
-    paths = find_lib('nvvm', libdir=libdir)
+    paths = find_lib("nvvm", libdir=libdir)
     if not paths:
         return
     # Use the directory name of the max path
@@ -109,16 +104,16 @@ def get_cuda_home(*subdirs):
     If *subdirs* are the subdirectory name to be appended in the resulting
     path.
     """
-    cuda_home = os.environ.get('CUDA_HOME')
+    cuda_home = os.environ.get("CUDA_HOME")
     if cuda_home is None:
         # Try Windows CUDA installation without Anaconda
-        cuda_home = os.environ.get('CUDA_PATH')
+        cuda_home = os.environ.get("CUDA_PATH")
     if cuda_home is not None:
         return os.path.join(cuda_home, *subdirs)
 
 
 def _get_nvvm_path():
     by, path = _get_nvvm_path_decision()
-    candidates = find_lib('nvvm', path)
+    candidates = find_lib("nvvm", path)
     path = max(candidates) if candidates else None
     return _env_path_tuple(by, path)
--- cuda_bindings/cuda/bindings/ecosystem/findlib.py
+++ cuda_bindings/cuda/bindings/ecosystem/findlib.py
@@ -1,41 +1,43 @@
 # Forked from:
 # https://github.com/numba/numba/blob/f0d24824fcd6a454827e3c108882395d00befc04/numba/misc/findlib.py
 
-import sys
 import os
 import re
+import sys
 
 
 def get_lib_dirs():
     """
     Anaconda specific
     """
-    if sys.platform == 'win32':
+    if sys.platform == "win32":
         # on windows, historically `DLLs` has been used for CUDA libraries,
         # since approximately CUDA 9.2, `Library\bin` has been used.
-        dirnames = ['DLLs', os.path.join('Library', 'bin')]
+        dirnames = ["DLLs", os.path.join("Library", "bin")]
     else:
-        dirnames = ['lib', ]
+        dirnames = [
+            "lib",
+        ]
     libdirs = [os.path.join(sys.prefix, x) for x in dirnames]
     return libdirs
 
 
 DLLNAMEMAP = {
-    'linux': r'lib%(name)s\.so\.%(ver)s$',
-    'linux2': r'lib%(name)s\.so\.%(ver)s$',
-    'linux-static': r'lib%(name)s\.a$',
-    'darwin': r'lib%(name)s\.%(ver)s\.dylib$',
-    'win32': r'%(name)s%(ver)s\.dll$',
-    'win32-static': r'%(name)s\.lib$',
-    'bsd': r'lib%(name)s\.so\.%(ver)s$',
+    "linux": r"lib%(name)s\.so\.%(ver)s$",
+    "linux2": r"lib%(name)s\.so\.%(ver)s$",
+    "linux-static": r"lib%(name)s\.a$",
+    "darwin": r"lib%(name)s\.%(ver)s\.dylib$",
+    "win32": r"%(name)s%(ver)s\.dll$",
+    "win32-static": r"%(name)s\.lib$",
+    "bsd": r"lib%(name)s\.so\.%(ver)s$",
 }
 
-RE_VER = r'[0-9]*([_\.][0-9]+)*'
+RE_VER = r"[0-9]*([_\.][0-9]+)*"
 
 
 def find_lib(libname, libdir=None, platform=None, static=False):
     platform = platform or sys.platform
-    platform = 'bsd' if 'bsd' in platform else platform
+    platform = "bsd" if "bsd" in platform else platform
     if static:
         platform = f"{platform}-static"
     if platform not in DLLNAMEMAP:
@@ -51,7 +53,9 @@ def find_file(pat, libdir=None):
     if libdir is None:
         libdirs = get_lib_dirs()
     elif isinstance(libdir, str):
-        libdirs = [libdir,]
+        libdirs = [
+            libdir,
+        ]
     else:
         libdirs = list(libdir)
     files = []
@@ -60,7 +64,6 @@ def find_file(pat, libdir=None):
             entries = os.listdir(ldir)
         except FileNotFoundError:
             continue
-        candidates = [os.path.join(ldir, ent)
-                      for ent in entries if pat.match(ent)]
+        candidates = [os.path.join(ldir, ent) for ent in entries if pat.match(ent)]
         files.extend([c for c in candidates if os.path.isfile(c)])
     return files
