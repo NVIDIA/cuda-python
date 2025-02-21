e180023d5bf57d362925830a47b05b6f3e1aa3eb
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-02-21 09:29:36 -0800

    Add test_runtime_error_info

--- cuda_core/tests/test_wip.py
+++ cuda_core/tests/test_wip.py
@@ -1,8 +1,8 @@
-from cuda.bindings import driver
+from cuda.bindings import driver, runtime
 from cuda.core.experimental import _utils
 
 
-def test_all():
+def test_driver_error_info():
     for code in range(1000):
         try:
             error = driver.CUresult(code)
@@ -13,6 +13,23 @@ def test_all():
             print(error)
             if code not in (226, 721, 916):  # These trigger SegFaults
                 name, desc, expl = _utils._driver_error_info(error)
+                print(name)
+                print(desc)
+                print(expl)
+            print()
+
+
+def test_runtime_error_info():
+    for code in range(100000):
+        try:
+            error = runtime.cudaError_t(code)
+        except ValueError:
+            pass
+        else:
+            print(code)
+            print(error)
+            if True:
+                name, desc, expl = _utils._runtime_error_info(error)
                 print(name)
                 print(desc)
                 print(expl)
