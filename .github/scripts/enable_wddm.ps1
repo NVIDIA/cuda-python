5bc6f9bdea1e3900a30110c46a09e9595e6ca79e
Leo Fang <leof@nvidia.com>
2025-02-23 22:13:31 -0500

    Update enable_wddm.ps1

--- .github/scripts/enable_wddm.ps1
+++ .github/scripts/enable_wddm.ps1
@@ -2,6 +2,6 @@
 
 Get-PnpDevice
 $gpu = Get-PnpDevice -FriendlyName 'NVIDIA*'
-nvidia-smi -i 0 -dm WDDM
+nvidia-smi -i 0 -fdm 0
 Disable-PnpDevice -InstanceId $gpu.InstanceId -Confirm:$false
 Enable-PnpDevice -InstanceId $gpu.InstanceId -Confirm:$false
