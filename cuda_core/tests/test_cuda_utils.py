bcc8cc6e9fd0095ffa9ea412c6b6b0c22a8adc60
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-03-08 11:07:10 -0800

    Add test_check_driver_error(), test_check_runtime_error()

--- cuda_core/tests/test_cuda_utils.py
+++ cuda_core/tests/test_cuda_utils.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
 
+import pytest
+
 from cuda.bindings import driver, runtime
 from cuda.core.experimental._utils import cuda_utils
 
@@ -36,3 +38,41 @@ def test_runtime_cuda_error_explanations_health():
         # Ensure expl_dict has no codes not known as a cudaError_t enum
         extra_expl = sorted(set(expl_dict.keys()) - known_codes)
         assert not extra_expl
+
+
+def test_check_driver_error():
+    num_unexpected = 0
+    for error in driver.CUresult:
+        if error == driver.CUresult.CUDA_SUCCESS:
+            assert cuda_utils._check_driver_error(error) is None
+        else:
+            with pytest.raises(cuda_utils.CUDAError) as e:
+                cuda_utils._check_driver_error(error)
+            msg = str(e)
+            if "UNEXPECTED ERROR CODE" in msg:
+                num_unexpected += 1
+            else:
+                # Example repr(error): <CUresult.CUDA_ERROR_UNKNOWN: 999>
+                enum_name = repr(error).split(".", 1)[1].split(":", 1)[0]
+                assert enum_name in msg
+    # Smoke test: We don't want most to be unexpected.
+    assert num_unexpected < len(driver.CUresult) * 0.5
+
+
+def test_check_runtime_error():
+    num_unexpected = 0
+    for error in runtime.cudaError_t:
+        if error == runtime.cudaError_t.cudaSuccess:
+            assert cuda_utils._check_runtime_error(error) is None
+        else:
+            with pytest.raises(cuda_utils.CUDAError) as e:
+                cuda_utils._check_runtime_error(error)
+            msg = str(e)
+            if "UNEXPECTED ERROR CODE" in msg:
+                num_unexpected += 1
+            else:
+                # Example repr(error): <cudaError_t.cudaErrorUnknown: 999>
+                enum_name = repr(error).split(".", 1)[1].split(":", 1)[0]
+                assert enum_name in msg
+    # Smoke test: We don't want most to be unexpected.
+    assert num_unexpected < len(driver.CUresult) * 0.5
