93a66b0e02a31543a89f489a867785aa6f8774a7
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-04-02 15:14:21 -0700

    Use win32api.LoadLibrary() instead of ctypes.windll.kernel32.LoadLibraryW(), to be more similar to original (and working) cython code.
    Hoping to resolve this kind of error:
    
    ```
    _ ERROR at setup of test_c_or_v_program_fail_bad_option[txt-compile_program] __
    
    request = <SubRequest 'minimal_nvvmir' for <Function test_c_or_v_program_fail_bad_option[txt-compile_program]>>
    
        @pytest.fixture(params=MINIMAL_NVVMIR_FIXTURE_PARAMS)
        def minimal_nvvmir(request):
            for pass_counter in range(2):
                nvvmir = MINIMAL_NVVMIR_CACHE.get(request.param, -1)
                if nvvmir != -1:
                    if nvvmir is None:
                        pytest.skip(f"UNAVAILABLE: {request.param}")
                    return nvvmir
                if pass_counter:
                    raise AssertionError("This code path is meant to be unreachable.")
                # Build cache entries, then try again (above).
    >           major, minor, debug_major, debug_minor = nvvm.ir_version()
    
    tests\test_nvvm.py:148:
    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
    cuda\bindings\nvvm.pyx:95: in cuda.bindings.nvvm.ir_version
        cpdef tuple ir_version():
    cuda\bindings\nvvm.pyx:113: in cuda.bindings.nvvm.ir_version
        status = nvvmIRVersion(&major_ir, &minor_ir, &major_dbg, &minor_dbg)
    cuda\bindings\cynvvm.pyx:19: in cuda.bindings.cynvvm.nvvmIRVersion
        return _nvvm._nvvmIRVersion(majorIR, minorIR, majorDbg, minorDbg)
    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
    
    >   ???
    E   cuda.bindings._internal.utils.FunctionNotFoundError: function nvvmIRVersion is not found
    ```

--- cuda_bindings/cuda/bindings/_path_finder_utils/load_nvidia_dynamic_library.py
+++ cuda_bindings/cuda/bindings/_path_finder_utils/load_nvidia_dynamic_library.py
@@ -1,21 +1,26 @@
-import ctypes
 import functools
-import os
 import sys
+
+if sys.platform == "win32":
+    import pywintypes
+    import win32api
+else:
+    import ctypes
+    import os
 
 from .find_nvidia_dynamic_library import find_nvidia_dynamic_library
 
 
 @functools.cache
 def load_nvidia_dynamic_library(name: str) -> int:
+    print(f"\nLOOOK load_nvidia_dynamic_library({name}) ENTRY", flush=True)
     dl_path = find_nvidia_dynamic_library(name)
     if sys.platform == "win32":
         try:
-            handle = ctypes.windll.kernel32.LoadLibraryW(dl_path)
-            if not handle:
-                raise ctypes.WinError(ctypes.get_last_error())
-        except Exception as e:
+            handle = win32api.LoadLibrary(dl_path)
+        except pywintypes.error as e:
             raise RuntimeError(f"Failed to load DLL at {dl_path}: {e}") from e
+        print(f"\nLOOOK load_nvidia_dynamic_library({name}) RETURN {type(handle)=} {handle=}", flush=True)
         # Use `cdef void* ptr = <void*><intptr_t>` in cython to convert back to void*
         return handle  # C signed int, matches win32api.GetProcAddress
     else:
@@ -23,5 +28,8 @@ def load_nvidia_dynamic_library(name: str) -> int:
             handle = ctypes.CDLL(dl_path, mode=os.RTLD_NOW | os.RTLD_GLOBAL)
         except OSError as e:
             raise RuntimeError(f"Failed to dlopen {dl_path}: {e}") from e
+        print(
+            f"\nLOOOK load_nvidia_dynamic_library({name}) RETURN {type(handle._handle)=} {handle._handle=}", flush=True
+        )
         # Use `cdef void* ptr = <void*><uintptr_t>` in cython to convert back to void*
         return handle._handle  # C unsigned int
