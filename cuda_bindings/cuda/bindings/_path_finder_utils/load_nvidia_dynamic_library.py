1944e4c7fd66d0b2fe25603d7efbc32163f8b748
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-04-02 15:40:28 -0700

    Remove debug print statements.

--- cuda_bindings/cuda/bindings/_path_finder_utils/load_nvidia_dynamic_library.py
+++ cuda_bindings/cuda/bindings/_path_finder_utils/load_nvidia_dynamic_library.py
@@ -13,14 +13,12 @@ from .find_nvidia_dynamic_library import find_nvidia_dynamic_library
 
 @functools.cache
 def load_nvidia_dynamic_library(name: str) -> int:
-    print(f"\nLOOOK load_nvidia_dynamic_library({name}) ENTRY", flush=True)
     dl_path = find_nvidia_dynamic_library(name)
     if sys.platform == "win32":
         try:
             handle = win32api.LoadLibrary(dl_path)
         except pywintypes.error as e:
             raise RuntimeError(f"Failed to load DLL at {dl_path}: {e}") from e
-        print(f"\nLOOOK load_nvidia_dynamic_library({name}) RETURN {type(handle)=} {handle=}", flush=True)
         # Use `cdef void* ptr = <void*><intptr_t>` in cython to convert back to void*
         return handle  # C signed int, matches win32api.GetProcAddress
     else:
@@ -28,8 +26,5 @@ def load_nvidia_dynamic_library(name: str) -> int:
             handle = ctypes.CDLL(dl_path, mode=os.RTLD_NOW | os.RTLD_GLOBAL)
         except OSError as e:
             raise RuntimeError(f"Failed to dlopen {dl_path}: {e}") from e
-        print(
-            f"\nLOOOK load_nvidia_dynamic_library({name}) RETURN {type(handle._handle)=} {handle._handle=}", flush=True
-        )
         # Use `cdef void* ptr = <void*><uintptr_t>` in cython to convert back to void*
         return handle._handle  # C unsigned int
