a603ef82436a1c82116ac53c87fda719611d287c
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-04-04 12:53:58 -0700

    Linux: First try using the platform-specific dynamic loader search mechanisms

--- cuda_bindings/cuda/bindings/_path_finder/load_nvidia_dynamic_library.py
+++ cuda_bindings/cuda/bindings/_path_finder/load_nvidia_dynamic_library.py
@@ -10,9 +10,24 @@ else:
 
 from .find_nvidia_dynamic_library import find_nvidia_dynamic_library
 
+_LINUX_CDLL_MODE = os.RTLD_NOW | os.RTLD_GLOBAL
+
 
 @functools.cache
 def load_nvidia_dynamic_library(name: str) -> int:
+    # First try using the platform-specific dynamic loader search mechanisms
+    if sys.platform == "win32":
+        pass  # TODO
+    else:
+        dl_path = f"lib{name}.so"  # Version intentionally no specified.
+        try:
+            handle = ctypes.CDLL(dl_path, _LINUX_CDLL_MODE)
+        except OSError:
+            pass
+        else:
+            # Use `cdef void* ptr = <void*><uintptr_t>` in cython to convert back to void*
+            return handle._handle  # C unsigned int
+
     dl_path = find_nvidia_dynamic_library(name)
     if sys.platform == "win32":
         try:
@@ -23,7 +38,7 @@ def load_nvidia_dynamic_library(name: str) -> int:
         return handle  # C signed int, matches win32api.GetProcAddress
     else:
         try:
-            handle = ctypes.CDLL(dl_path, mode=os.RTLD_NOW | os.RTLD_GLOBAL)
+            handle = ctypes.CDLL(dl_path, _LINUX_CDLL_MODE)
         except OSError as e:
             raise RuntimeError(f"Failed to dlopen {dl_path}: {e}") from e
         # Use `cdef void* ptr = <void*><uintptr_t>` in cython to convert back to void*
