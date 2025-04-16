cdf8f9333d34fa3482c7a81452e09a7e1f77ba3a
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-04-15 20:14:09 -0700

    Change test_path_finder::test_find_and_load() to skip cufile on Windows, and report exceptions as failures, except for cudart

--- cuda_bindings/tests/test_path_finder.py
+++ cuda_bindings/tests/test_path_finder.py
@@ -1,3 +1,5 @@
+import sys
+
 import pytest
 
 from cuda.bindings import path_finder
@@ -9,11 +11,20 @@ def test_supported_libnames_windows_dlls_consistency():
 
 @pytest.mark.parametrize("libname", path_finder.SUPPORTED_LIBNAMES)
 def test_find_and_load(libname):
-    print(f"\n{libname}")
-    for fun in (path_finder.find_nvidia_dynamic_library, path_finder.load_nvidia_dynamic_library):
+    if sys.platform == "win32" and libname == "cufile":
+        pytest.skip(f'test_find_and_load("{libname}") not supported on this platform')
+    print(f'\ntest_find_and_load("{libname}")')
+    failures = []
+    for algo, func in (
+        ("find", path_finder.find_nvidia_dynamic_library),
+        ("load", path_finder.load_nvidia_dynamic_library),
+    ):
         try:
-            out = fun(libname)
+            out = func(libname)
         except Exception as e:
             out = f"EXCEPTION: {type(e)} {str(e)}"
+            if libname != "cudart":
+                failures.append(algo)
         print(out)
     print()
+    assert not failures
