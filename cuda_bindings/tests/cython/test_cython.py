e87e8f5b1a3b93a74441af23b122157f4d5379c7
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-05-12 15:23:31 -0700

    Remove all EULA paragraphs (cuda_bindings/, cuda_core/)

--- cuda_bindings/tests/cython/test_cython.py
+++ cuda_bindings/tests/cython/test_cython.py
@@ -1,11 +1,6 @@
 # Copyright 2021-2024 NVIDIA Corporation.  All rights reserved.
 # SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
-#
-# Please refer to the NVIDIA end user license agreement (EULA) associated
-# with this source code for terms and conditions that govern your use of
-# this software. Any use, reproduction, disclosure, or distribution of
-# this software and related documentation outside the terms of the EULA
-# is strictly prohibited.
+
 import functools
 import importlib
 import sys
--- cuda_core/tests/test_launcher.py
+++ cuda_core/tests/test_launcher.py
@@ -1,11 +1,5 @@
 # Copyright 2024 NVIDIA Corporation.  All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-#
-# Please refer to the NVIDIA end user license agreement (EULA) associated
-# with this source code for terms and conditions that govern your use of
-# this software. Any use, reproduction, disclosure, or distribution of
-# this software and related documentation outside the terms of the EULA
-# is strictly prohibited.
 
 import pytest
 
