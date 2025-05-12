7635cbce2036239dca0c3431842730769e9ab790
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-05-12 15:00:44 -0700

    Add missing Copyright (and SPDX-License-Identifier) lines under cuda_bindings/

--- cuda_bindings/cuda/ccudart.pyx
+++ cuda_bindings/cuda/ccudart.pyx
@@ -1,3 +1,6 @@
+# Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
+
 from cuda.bindings.cyruntime cimport *
 from cuda.bindings import cyruntime
 __pyx_capi__ = cyruntime.__pyx_capi__
