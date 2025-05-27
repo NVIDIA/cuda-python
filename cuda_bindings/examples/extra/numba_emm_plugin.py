6413babbfcabec64a0943ad46e49387309af7fb7
Vladislav Zhurba <vzhurba@nvidia.com>
2025-05-27 14:56:32 -0700

    Update license headers on examples

--- cuda_bindings/examples/extra/numba_emm_plugin.py
+++ cuda_bindings/examples/extra/numba_emm_plugin.py
@@ -1,10 +1,5 @@
 # Copyright 2021-2025 NVIDIA Corporation.  All rights reserved.
-#
-# Please refer to the NVIDIA end user license agreement (EULA) associated
-# with this source code for terms and conditions that govern your use of
-# this software. Any use, reproduction, disclosure, or distribution of
-# this software and related documentation outside the terms of the EULA
-# is strictly prohibited.
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
 
 """Numba EMM Plugin using the CUDA Python Driver API.
 
