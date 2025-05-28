5ed86c56b0743f4aee8a9d0077c56e307c0b59d2
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-05-28 10:09:11 -0700

    Add tests/test_path_finder_find_headers.py, with hard-coded paths.

---
+++ cuda_bindings/tests/test_path_finder_find_headers.py
@@ -0,0 +1,24 @@
+# Copyright 2025 NVIDIA Corporation.  All rights reserved.
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
+
+import pytest
+
+from cuda.bindings._path_finder import find_nvidia_headers
+
+
+def test_find_nvidia_header_directory(info_summary_append):
+    with pytest.raises(AssertionError):
+        find_nvidia_headers.find_nvidia_header_directory("unknown-libname")
+
+    hdr_dir = find_nvidia_headers.find_nvidia_header_directory("nvshmem")
+    assert hdr_dir in [
+        # pip install nvidia-nvshmem-cu12
+        "/home/rgrossekunst/forked/cuda-python/venvs/scratch/lib/python3.12/site-packages/nvidia/nvshmem/include",
+        # conda create -y -n nvshmem python=3.12
+        # conda activate nvshmem
+        # conda install -y conda-forge::libnvshmem3 conda-forge::libnvshmem-dev
+        "/home/rgrossekunst/miniforge3/envs/nvshmem/include",
+        # sudo apt install libnvshmem3-cuda-12 libnvshmem3-dev-cuda-12
+        "/usr/include/nvshmem_12",
+    ]
+    info_summary_append(f"{hdr_dir=!r}")
