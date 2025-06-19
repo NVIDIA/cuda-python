1d5d5ee6c8610183f32da35b7d1ad1a11c1774b1
Ashwin Srinath <shwina@users.noreply.github.com>
2025-06-19 15:50:14 -0400

    Load nvrtc using path_finder

--- cuda_core/cuda/core/__init__.py
+++ cuda_core/cuda/core/__init__.py
@@ -2,4 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from cuda.bindings.path_finder import _load_nvidia_dynamic_library
 from cuda.core._version import __version__
+
+""" _load_nvidia_dynamic_library("nvrtc") """
