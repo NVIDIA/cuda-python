30de7203b98f5e3f1e67f37ea8182eca08df7b9c
Oleksandr Pavlyk <21087696+oleksandr-pavlyk@users.noreply.github.com>
2025-06-30 13:04:37 -0500

    Verify that stream.context handle is not None in test_stream_context

--- cuda_core/tests/test_stream.py
+++ cuda_core/tests/test_stream.py
@@ -80,6 +80,7 @@ def test_stream_context(init_cuda):
     stream = Device().create_stream(options=StreamOptions())
     context = stream.context
     assert context is not None
+    assert context._handle is not None
 
 
 def test_stream_from_foreign_stream(init_cuda):
