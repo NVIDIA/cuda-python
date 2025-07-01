50f4f8eade53d8cfd4ce2cc53fc61be7fa023e82
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-07-01 05:25:19 -0700

    Use suggested version number 0.1.0

--- cuda_pathfinder/cuda/pathfinder/_version.py
+++ cuda_pathfinder/cuda/pathfinder/_version.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
