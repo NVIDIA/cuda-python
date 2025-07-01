7ccf852b99740ebc38538f5aab8680176916fd3d
Ralf W. Grosse-Kunstleve <rwgkio@gmail.com>
2025-07-01 16:30:09 -0700

    cuda_core forward compatibility changes. (#722)
    * cuda_core forward compatibility changes.
    
    * Add missing file.
    
    * Fix logic error computing CCCL_INCLUDE_PATHS in cuda_core/tests/helpers.py

--- cuda_core/tests/test_event.py
+++ cuda_core/tests/test_event.py
@@ -6,6 +6,7 @@ import pathlib
 import platform
 import time
 
+import helpers
 import numpy as np
 import pytest
 from conftest import skipif_need_cuda_headers
@@ -149,7 +150,7 @@ __global__ void wait(int* val) {
     program_options = ProgramOptions(
         std="c++17",
         arch=f"sm_{arch}",
-        include_path=str(pathlib.Path(os.environ["CUDA_PATH"]) / pathlib.Path("include")),
+        include_path=helpers.CCCL_INCLUDE_PATHS,
     )
     prog = Program(code, code_type="c++", options=program_options)
     mod = prog.compile(target_type="cubin")
