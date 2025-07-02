36491c88a7639a655a9d6c153a4b36a366a85c6f
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-07-02 10:54:58 -0700

    git mv nvidia_dynamic_libs.py __init__.py

--- cuda_pathfinder/cuda/pathfinder/__init__.py
+++ cuda_pathfinder/cuda/pathfinder/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from cuda.pathfinder.nvidia_dynamic_libs import SUPPORTED_NVIDIA_LIBNAMES, LoadedDL, load_nvidia_dynamic_lib
+from cuda.pathfinder._dynamic_libs.load_dl_common import LoadedDL
+from cuda.pathfinder._dynamic_libs.load_nvidia_dynamic_lib import load_nvidia_dynamic_lib
+from cuda.pathfinder._dynamic_libs.supported_nvidia_libs import SUPPORTED_LIBNAMES as SUPPORTED_NVIDIA_LIBNAMES
 
 __all__ = ["LoadedDL", "load_nvidia_dynamic_lib", "SUPPORTED_NVIDIA_LIBNAMES"]
