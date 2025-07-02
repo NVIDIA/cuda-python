7742c5c75d46ab4ee100380db7c68f1692aa682f
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-07-02 12:22:25 -0700

    Import __version__ in cuda/pathfinder/__init__.py

--- cuda_pathfinder/cuda/pathfinder/__init__.py
+++ cuda_pathfinder/cuda/pathfinder/__init__.py
@@ -4,5 +4,6 @@
 from cuda.pathfinder._dynamic_libs.load_dl_common import LoadedDL
 from cuda.pathfinder._dynamic_libs.load_nvidia_dynamic_lib import load_nvidia_dynamic_lib
 from cuda.pathfinder._dynamic_libs.supported_nvidia_libs import SUPPORTED_LIBNAMES as SUPPORTED_NVIDIA_LIBNAMES
+from cuda.pathfinder._version import __version__
 
-__all__ = ["LoadedDL", "load_nvidia_dynamic_lib", "SUPPORTED_NVIDIA_LIBNAMES"]
+__all__ = ["LoadedDL", "load_nvidia_dynamic_lib", "SUPPORTED_NVIDIA_LIBNAMES", "__version__"]
