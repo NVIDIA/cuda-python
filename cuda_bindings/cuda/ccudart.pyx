533fd877f65e1f2defc76e76cf37a6255137a4a4
Leo Fang <leof@nvidia.com>
2025-08-06 16:41:38 +0000

    remove trampoline modules

--- cuda_bindings/cuda/ccudart.pyx
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
-
-from cuda.bindings.cyruntime cimport *
-from cuda.bindings import cyruntime
-__pyx_capi__ = cyruntime.__pyx_capi__
-del cyruntime
