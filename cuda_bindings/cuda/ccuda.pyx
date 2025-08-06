c4f4f401b9b30d537cbe6dddbc678cb8e694e573
Leo Fang <leof@nvidia.com>
2025-08-07 00:09:59 +0800

    Remove trampoline modules (#795)
    * remove trampoline modules
    
    * try to also clean up MANIFEST.in
    
    * Revert "try to also clean up MANIFEST.in"
    
    This reverts commit 0ff9250f51dc0b24756d130cce30af117f7368fc.
    
    * add release notes

--- cuda_bindings/cuda/ccuda.pyx
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
-
-from cuda.bindings.cydriver cimport *
-from cuda.bindings import cydriver
-__pyx_capi__ = cydriver.__pyx_capi__
-del cydriver
--- cuda_bindings/cuda/cnvrtc.pyx
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
-
-from cuda.bindings.cynvrtc cimport *
-from cuda.bindings import cynvrtc
-__pyx_capi__ = cynvrtc.__pyx_capi__
-del cynvrtc
