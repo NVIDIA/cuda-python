c3464b31ae273f6f7e80577be444f62b06f9ebe3
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-08-11 23:20:02 -0700

    test_find_nvidia_headers.py: removed hard-wired paths, comments with more complete commands for setting up manual testing.

--- cuda_pathfinder/tests/test_find_nvidia_headers.py
+++ cuda_pathfinder/tests/test_find_nvidia_headers.py
@@ -11,20 +11,19 @@ def test_find_nvidia_header_directory(info_summary_append):
         find_nvidia_header_directory("unknown-libname")
 
     hdr_dir = find_nvidia_header_directory("nvshmem")
-    # TODO: Find ways to test more meaningfully, and how to avoid HARD-WIRED PATHS in particular.
-    assert hdr_dir in [
-        # pip install nvidia-nvshmem-cu12
-        "/home/rgrossekunst/forked/cuda-python/venvs/scratch/lib/python3.12/site-packages/nvidia/nvshmem/include",
-        #
-        # conda create -y -n nvshmem python=3.12
-        # conda activate nvshmem
-        # conda install -y conda-forge::libnvshmem3 conda-forge::libnvshmem-dev
-        "/home/rgrossekunst/miniforge3/envs/nvshmem/include",
-        #
-        # sudo apt install libnvshmem3-cuda-12 libnvshmem3-dev-cuda-12
-        "/usr/include/nvshmem_12",
-        #
-        # nvshmem not available
-        None,
-    ]
+    # TODO: Find ways to test more meaningfully.
+
+    # pip install nvidia-nvshmem-cu12
+    # pip install nvidia-nvshmem-cu13
+
+    # conda create -y -n nvshmem python=3.12
+    # conda activate nvshmem
+    # conda install -y conda-forge::libnvshmem3 conda-forge::libnvshmem-dev
+
+    # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+    # sudo dpkg -i cuda-keyring_1.1-1_all.deb
+    # sudo apt update
+    # sudo apt install libnvshmem3-cuda-12 libnvshmem3-dev-cuda-12
+    # sudo apt install libnvshmem3-cuda-13 libnvshmem3-dev-cuda-13
+
     info_summary_append(f"{hdr_dir=!r}")
