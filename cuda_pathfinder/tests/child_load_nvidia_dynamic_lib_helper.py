1b216d7eca43c5506f0bc8b9c13a291071920545
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-08-21 14:01:56 -0700

    Replace special-case workaround in tests/child_load_nvidia_dynamic_lib_helper.py with a more general approach.

--- cuda_pathfinder/tests/child_load_nvidia_dynamic_lib_helper.py
+++ cuda_pathfinder/tests/child_load_nvidia_dynamic_lib_helper.py
@@ -26,6 +26,11 @@ def validate_abs_path(abs_path):
 def child_process_func(libname):
     from cuda.pathfinder import load_nvidia_dynamic_lib
     from cuda.pathfinder._dynamic_libs.load_nvidia_dynamic_lib import _load_lib_no_cache
+    from cuda.pathfinder._dynamic_libs.supported_nvidia_libs import (
+        IS_WINDOWS,
+        SUPPORTED_LINUX_SONAMES,
+        SUPPORTED_WINDOWS_DLLS,
+    )
 
     loaded_dl_fresh = load_nvidia_dynamic_lib(libname)
     if loaded_dl_fresh.was_already_loaded_from_elsewhere:
@@ -37,11 +42,9 @@ def child_process_func(libname):
         raise RuntimeError("loaded_dl_from_cache is not loaded_dl_fresh")
 
     loaded_dl_no_cache = _load_lib_no_cache(libname)
-    if (
-        not loaded_dl_no_cache.was_already_loaded_from_elsewhere
-        # Special case: foreign_wheels
-        and not libname.startswith("git2")
-    ):
+    # check_if_already_loaded_from_elsewhere relies on these:
+    supported_libs = SUPPORTED_WINDOWS_DLLS if IS_WINDOWS else SUPPORTED_LINUX_SONAMES
+    if not loaded_dl_no_cache.was_already_loaded_from_elsewhere and libname in supported_libs:
         raise RuntimeError("not loaded_dl_no_cache.was_already_loaded_from_elsewhere")
     if not os.path.samefile(loaded_dl_no_cache.abs_path, loaded_dl_fresh.abs_path):
         raise RuntimeError(f"not os.path.samefile({loaded_dl_no_cache.abs_path=!r}, {loaded_dl_fresh.abs_path=!r})")
