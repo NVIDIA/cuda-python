80ee3c9970f20b87a188fa1279431b0f6d4dd504
Andy Jost <ajost@nvidia.com>
2025-09-15 11:20:18 -0700

    Reworks mempool attribute implementation.

--- cuda_core/cuda/core/experimental/_memory.pyx
+++ cuda_core/cuda/core/experimental/_memory.pyx
@@ -487,12 +487,11 @@ class DeviceMemoryResourceAttributes:
         self._mr = mr
         return self
 
-    def _defproperty(scope: dict, name: str, property_type: type, doc: str):
+    def _getter(name: str, property_type: type):
         # Define a property in the supplied scope for accessing a memory pool attribute.
         #
         # Args:
         #     name: The name of the property (e.g., 'reuse_follow_event_dependencies').
-        #     doc: The docstring for the property.
         #     property_type: The return type of the property (e.g., bool, int).
         #
         # Returns:
@@ -500,27 +499,25 @@ class DeviceMemoryResourceAttributes:
 
         attr_enum = getattr(driver.CUmemPool_attribute, f"CU_MEMPOOL_ATTR_{name.upper()}")
 
-        def getter(self) -> property_type:
+        def impl(self) -> property_type:
             mr = self._mr()
             if mr is None:
               raise RuntimeError("DeviceMemoryResource is expired")
             err, value = driver.cuMemPoolGetAttribute(mr._mempool_handle, attr_enum)
             raise_if_driver_error(err)
             return property_type(value)
+        return impl
 
-        getter.__doc__ = doc
-        scope[name] = property(getter)
+    reuse_follow_event_dependencies = property(fget=_getter("reuse_follow_event_dependencies", bool), doc="Allow memory to be reused when there are event dependencies between streams.")
+    reuse_allow_opportunistic = property(fget=_getter("reuse_allow_opportunistic", bool), doc="Allow reuse of completed frees without dependencies.")
+    reuse_allow_internal_dependencies = property(fget=_getter("reuse_allow_internal_dependencies", bool), doc="Allow insertion of new stream dependencies for memory reuse.")
+    release_threshold = property(fget=_getter("release_threshold", int), doc="Amount of reserved memory to hold before OS release.")
+    reserved_mem_current = property(fget=_getter("reserved_mem_current", int), doc="Current amount of backing memory allocated.")
+    reserved_mem_high = property(fget=_getter("reserved_mem_high", int), doc="High watermark of backing memory allocated.")
+    used_mem_current = property(fget=_getter("used_mem_current", int), doc="Current amount of memory in use.")
+    used_mem_high = property(fget=_getter("used_mem_high", int), doc="High watermark of memory in use.")
 
-    _defproperty(locals(), "reuse_follow_event_dependencies", bool, "Allow memory to be reused when there are event dependencies between streams.")
-    _defproperty(locals(), "reuse_allow_opportunistic", bool, "Allow reuse of completed frees without dependencies.")
-    _defproperty(locals(), "reuse_allow_internal_dependencies", bool, "Allow insertion of new stream dependencies for memory reuse.")
-    _defproperty(locals(), "release_threshold", int, "Amount of reserved memory to hold before OS release.")
-    _defproperty(locals(), "reserved_mem_current", int, "Current amount of backing memory allocated.")
-    _defproperty(locals(), "reserved_mem_high", int, "High watermark of backing memory allocated.")
-    _defproperty(locals(), "used_mem_current", int, "Current amount of memory in use.")
-    _defproperty(locals(), "used_mem_high", int, "High watermark of memory in use.")
-
-    del _defproperty
+    del _getter
 
 
 class DeviceMemoryResource(MemoryResource):
