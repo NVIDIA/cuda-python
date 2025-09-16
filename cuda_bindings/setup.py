2380fda82d1f372de2c4005f03d0c0316ddfb66e
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2025-09-16 13:49:24 -0400

    chore: fmt

--- cuda_bindings/setup.py
+++ cuda_bindings/setup.py
@@ -150,7 +150,7 @@ def parse_headers(header_dict):
         "\\(enum ": "(",
         # Since we only support 64 bit architectures, we can inline the sizeof(T*) to 8 and then compute the
         # result in Python. The arithmetic expression is preserved to help with clarity and understanding
-        r'char reserved\[52 - sizeof\(CUcheckpointGpuPair \*\)\];' : rf'char reserved[{52 - 8}];',
+        r"char reserved\[52 - sizeof\(CUcheckpointGpuPair \*\)\];": rf"char reserved[{52 - 8}];",
     }
 
     print(f'Parsing headers in "{include_path_list}" (Caching = {PARSER_CACHING})')
