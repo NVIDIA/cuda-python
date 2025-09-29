c1dc84d07b62fdcb878643379dbbfa818902fe3a
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2025-09-29 16:50:39 +0000

    chore: fix misspelling

--- cuda_pathfinder/cuda/pathfinder/_utils/platform_aware.py
+++ cuda_pathfinder/cuda/pathfinder/_utils/platform_aware.py
@@ -8,7 +8,7 @@ IS_WINDOWS = sys.platform == "win32"
 
 def quote_for_shell(s: str) -> str:
     if IS_WINDOWS:
-        # This is a relatively heavy import; keep pathfinder if possible.
+        # This is a relatively heavy import; keep pathfinder lean if possible.
         from subprocess import list2cmdline
 
         return list2cmdline([s])
