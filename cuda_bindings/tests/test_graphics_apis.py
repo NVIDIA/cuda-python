7b1057f558285c977727bd62ce5a11dbcf7f5f27
Michael Droettboom <mdboom@gmail.com>
2025-10-10 15:34:24 -0400

    Update cuda_bindings/tests/test_graphics_apis.py
    Co-authored-by: Leo Fang <leo80042@gmail.com>

--- cuda_bindings/tests/test_graphics_apis.py
+++ cuda_bindings/tests/test_graphics_apis.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2021-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
 
 from cuda.bindings import runtime as cudart
