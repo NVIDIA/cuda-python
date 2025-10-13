4c25b0d0c370c353a08efefce67a1a657f759091
Michael Droettboom <mdboom@gmail.com>
2025-10-13 08:57:03 -0400

    Improve tests

--- cuda_bindings/tests/test_graphics_apis.py
+++ cuda_bindings/tests/test_graphics_apis.py
@@ -6,32 +6,30 @@ from cuda.bindings import runtime as cudart
 
 
 def test_graphics_api_smoketest():
-    _ = pytest.importorskip("PySide6")
-    from PySide6 import QtGui, QtOpenGL
+    pyglet = pytest.importorskip("pyglet")
 
-    class GLWidget(QtOpenGL.QOpenGLWindow):
-        def initializeGL(self):
-            self.m_texture = QtOpenGL.QOpenGLTexture(QtOpenGL.QOpenGLTexture.Target.Target2D)
-            self.m_texture.setFormat(QtOpenGL.QOpenGLTexture.TextureFormat.RGBA8_UNorm)
-            self.m_texture.setSize(512, 512)
-            self.m_texture.allocateStorage()
+    tex = pyglet.image.Texture.create(512, 512)
 
-            err, self.gfx_resource = cudart.cudaGraphicsGLRegisterImage(
-                self.m_texture.textureId(),
-                self.m_texture.target().value,
-                cudart.cudaGraphicsRegisterFlags.cudaGraphicsRegisterFlagsWriteDiscard,
-            )
-            error_name = cudart.cudaGetErrorName(err)[1].decode()
+    err, gfx_resource = cudart.cudaGraphicsGLRegisterImage(
+        tex.id, tex.target, cudart.cudaGraphicsRegisterFlags.cudaGraphicsRegisterFlagsWriteDiscard
+    )
+    error_name = cudart.cudaGetErrorName(err)[1].decode()
+    if error_name == "cudaSuccess":
+        assert int(gfx_resource) != 0
+    else:
+        assert error_name in ("cudaErrorInvalidValue", "cudaErrorUnknown")
 
-            # We either have everything set up correctly and we get a gfx_resource,
-            # or we get an error.  Either way, we know the API actually did something,
-            # which is enough for this basic smoketest.
-            if error_name == "cudaSuccess":
-                assert int(self.gfx_resource) != 0
-            else:
-                assert error_name == "cudaErrorInvalidValue"
 
-    app = QtGui.QGuiApplication([])
-    win = GLWidget()
-    win.initializeGL()
-    win.show()
+def test_cuda_register_image_invalid():
+    """Exercise cudaGraphicsGLRegisterImage with dummy handle only using CUDA runtime API."""
+    fake_gl_texture_id = 1
+    fake_gl_target = 0x0DE1
+    flags = cudart.cudaGraphicsRegisterFlags.cudaGraphicsRegisterFlagsWriteDiscard
+
+    err, resource = cudart.cudaGraphicsGLRegisterImage(fake_gl_texture_id, fake_gl_target, flags)
+    err_name = cudart.cudaGetErrorName(err)[1].decode()
+    err_str = cudart.cudaGetErrorString(err)[1].decode()
+
+    if err == 0:
+        cudart.cudaGraphicsUnregisterResource(resource)
+        raise AssertionError("Expected error from invalid GL texture ID")
