25fe07e8895de71f9e14c867e27bf727aa77b634
Leo Fang <leof@nvidia.com>
2025-10-21 14:03:19 +0000

    bump to 12.9.4

--- cuda_bindings/cuda/bindings/_version.py
+++ cuda_bindings/cuda/bindings/_version.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
 
-__version__ = "12.9.3"
+__version__ = "12.9.4"
