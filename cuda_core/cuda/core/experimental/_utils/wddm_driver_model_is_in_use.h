85a1550ee989d446d72ca34c99f45311934cc3e6
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-11-22 22:56:48 -0800

    Add documentation comment to wddm_driver_model_is_in_use.h
    Add a comment block documenting the return codes, matching the style
    of hags_status.h. Also converts the file from binary to text mode
    per .gitattributes rules.

--- cuda_core/cuda/core/experimental/_utils/wddm_driver_model_is_in_use.h
+++ cuda_core/cuda/core/experimental/_utils/wddm_driver_model_is_in_use.h
@@ -7,6 +7,15 @@
 extern "C" {
 #endif
 
+/*
+ * wddm_driver_model_is_in_use
+ *
+ * Return codes:
+ *   -2 : Failed to get device count from NVML
+ *   -1 : Not available on this platform (not compiled with MSVC on Windows) or NVML initialization failed
+ *    0 : No WDDM driver model found (all devices use TCC or other driver models)
+ *    1 : WDDM driver model is in use (at least one device uses WDDM)
+ */
 int wddm_driver_model_is_in_use(void);
 
 #ifdef __cplusplus
