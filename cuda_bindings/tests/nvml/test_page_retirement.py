4886636b572e80d74a40fc0a352c9fb762767592
Michael Droettboom <mdboom@gmail.com>
2025-12-09 21:35:20 -0500

    Queue up 'skip reasons' (#1352)

--- cuda_bindings/tests/nvml/test_page_retirement.py
+++ cuda_bindings/tests/nvml/test_page_retirement.py
@@ -20,42 +20,55 @@ def supports_page_retirement(device):
         return False
 
 
-def test_page_retirement_notsupported(for_all_devices):
+def test_page_retirement_notsupported(all_devices):
     """
     Verifies that on platforms that don't supports page retirement, APIs will return Not Supported
     """
-    device = for_all_devices
+    skip_reasons = set()
 
-    if supports_page_retirement(device):
-        pytest.skip("page_retirement not supported")
+    for device in all_devices:
+        if supports_page_retirement(device):
+            skip_reasons.add(f"page_retirement is supported for {device}")
+            continue
 
-    if not util.supports_ecc(device):
-        pytest.skip("device doesn't support ECC")
+        if not util.supports_ecc(device):
+            skip_reasons.add(f"device doesn't support ECC for {device}")
+            continue
 
-    with pytest.raises(nvml.NotSupportedError):
-        for source in PAGE_RETIREMENT_PUBLIC_CAUSE_TYPES:
-            nvml.device_get_retired_pages(device, source)
+        with pytest.raises(nvml.NotSupportedError):
+            for source in PAGE_RETIREMENT_PUBLIC_CAUSE_TYPES:
+                nvml.device_get_retired_pages(device, source)
 
-    with pytest.raises(nvml.NotSupportedError):
-        nvml.device_get_retired_pages_pending_status(device)
+        with pytest.raises(nvml.NotSupportedError):
+            nvml.device_get_retired_pages_pending_status(device)
+
+    if skip_reasons:
+        pytest.skip(" ; ".join(skip_reasons))
 
 
-def test_page_retirement_supported(for_all_devices):
+def test_page_retirement_supported(all_devices):
     """
     Verifies that on platforms that support page_retirement, APIs will return success
     """
-    device = for_all_devices
+    skip_reasons = set()
 
-    if not supports_page_retirement(device):
-        pytest.skip("page_retirement not supported")
+    for device in all_devices:
+        if not supports_page_retirement(device):
+            skip_reasons.add(f"page_retirement not supported for {device}")
+            continue
 
-    if not util.supports_ecc(device):
-        pytest.skip("device doesn't support ECC")
+        if not util.supports_ecc(device):
+            skip_reasons.add(f"device doesn't support ECC for {device}")
+            continue
 
-    try:
-        for source in PAGE_RETIREMENT_PUBLIC_CAUSE_TYPES:
-            nvml.device_get_retired_pages(device, source)
-    except nvml.NotSupportedError:
-        pytest.skip("Exception case: Page retirment is not supported in this GPU")
+        try:
+            for source in PAGE_RETIREMENT_PUBLIC_CAUSE_TYPES:
+                nvml.device_get_retired_pages(device, source)
+        except nvml.NotSupportedError:
+            skip_reasons.add(f"Exception case: Page retirement is not supported in this GPU {device}")
+            continue
 
-    nvml.device_get_retired_pages_pending_status(device)
+        nvml.device_get_retired_pages_pending_status(device)
+
+    if skip_reasons:
+        pytest.skip(" ; ".join(skip_reasons))
