5f2480cdfe13cd6bc3c77f19cacdb88664eb3158
Kamil Tokarski <ktokarski@nvidia.com>
2025-12-12 00:54:27 +0100

    Add missing copyright header
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- cuda_core/tests/test_strided_memory_view_copy.py
+++ cuda_core/tests/test_strided_memory_view_copy.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
 import ctypes
 import random
 from enum import Enum
