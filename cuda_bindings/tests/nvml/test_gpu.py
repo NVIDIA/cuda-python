b972c8e48aa76b1da1c74ee99640ac6644408138
Michael Droettboom <mdboom@gmail.com>
2025-12-11 08:38:13 -0500

    Add experimental NVML bindings to 12.9.x branch (#1349)
    * Add experimental NVML bindings to 12.9.x branch
    
    * Add tests from main to 12.9.x branch
    
    * Remove newer APIs
    
    * Remove hand-written 13.0 bindings
    
    * Queue up 'skip reasons' (#1352)
    
    * Handle backport correctly
    
    * Fix cimport
    
    * More versioned struct fixes
    
    * Fix test for 12.9
    
    * Add 13.1 bindings

---
+++ cuda_bindings/tests/nvml/test_gpu.py
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
+
+import pytest
+
+from cuda.bindings import _nvml as nvml
+
+from . import util
+
+
+def test_gpu_get_module_id(nvml_init):
+    # Unique module IDs cannot exceed the number of GPUs on the system
+    device_count = nvml.device_get_count_v2()
+
+    for i in range(device_count):
+        device = nvml.device_get_handle_by_index_v2(i)
+        uuid = nvml.device_get_uuid(device)
+
+        if util.is_vgpu(device):
+            continue
+
+        module_id = nvml.device_get_module_id(device)
+        assert isinstance(module_id, int)
+
+
+def test_gpu_get_platform_info(all_devices):
+    skip_reasons = set()
+    for device in all_devices:
+        if util.is_vgpu(device):
+            skip_reasons.add(f"Not supported on vGPU device {device}")
+            continue
+
+        # TODO
+        # if device.feature_dict.board.chip < board_class.Architecture.Blackwell:
+        #     test_utils.skip_test("Not supported on chip before Blackwell")
+
+        try:
+            platform_info = nvml.device_get_platform_info(device)
+        except nvml.NotSupportedError:
+            skip_reasons.add(f"Not supported returned, linkely NVLink is disable for {device}")
+            continue
+
+        assert isinstance(platform_info, (nvml.PlatformInfo_v2, nvml.PlatformInfo_v1))
+
+    if skip_reasons:
+        pytest.skip(" ; ".join(skip_reasons))
---
+++ cuda_bindings/tests/nvml/test_page_retirement.py
@@ -0,0 +1,75 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
+
+import pytest
+
+from cuda.bindings import _nvml as nvml
+
+from . import util
+
+PAGE_RETIREMENT_PUBLIC_CAUSE_TYPES = list(range(nvml.PageRetirementCause.COUNT))
+
+
+def supports_page_retirement(device):
+    try:
+        for source in range(nvml.PageRetirementCause.COUNT):
+            nvml.device_get_retired_pages(device, source)
+        return True
+    except nvml.NotSupportedError as e:
+        return False
+    except nvml.FunctionNotFoundError as e:
+        return False
+
+
+def test_page_retirement_notsupported(all_devices):
+    """
+    Verifies that on platforms that don't supports page retirement, APIs will return Not Supported
+    """
+    skip_reasons = set()
+
+    for device in all_devices:
+        if supports_page_retirement(device):
+            skip_reasons.add(f"page_retirement is supported for {device}")
+            continue
+
+        if not util.supports_ecc(device):
+            skip_reasons.add(f"device doesn't support ECC for {device}")
+            continue
+
+        with pytest.raises(nvml.NotSupportedError):
+            for source in PAGE_RETIREMENT_PUBLIC_CAUSE_TYPES:
+                nvml.device_get_retired_pages(device, source)
+
+        with pytest.raises(nvml.NotSupportedError):
+            nvml.device_get_retired_pages_pending_status(device)
+
+    if skip_reasons:
+        pytest.skip(" ; ".join(skip_reasons))
+
+
+def test_page_retirement_supported(all_devices):
+    """
+    Verifies that on platforms that support page_retirement, APIs will return success
+    """
+    skip_reasons = set()
+
+    for device in all_devices:
+        if not supports_page_retirement(device):
+            skip_reasons.add(f"page_retirement not supported for {device}")
+            continue
+
+        if not util.supports_ecc(device):
+            skip_reasons.add(f"device doesn't support ECC for {device}")
+            continue
+
+        try:
+            for source in PAGE_RETIREMENT_PUBLIC_CAUSE_TYPES:
+                nvml.device_get_retired_pages(device, source)
+        except nvml.NotSupportedError:
+            skip_reasons.add(f"Exception case: Page retirement is not supported in this GPU {device}")
+            continue
+
+        nvml.device_get_retired_pages_pending_status(device)
+
+    if skip_reasons:
+        pytest.skip(" ; ".join(skip_reasons))
