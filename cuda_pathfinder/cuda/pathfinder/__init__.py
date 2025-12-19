008b4291cea0227c596322adf21ceba587cdae8d
Michael Droettboom <mdboom@gmail.com>
2025-12-19 14:01:43 -0500

    Backport #1411 (#1415)
    * Backport #1411
    
    * Search for cuda_bindings wheel locally
    
    * Secondary fixes
    
    * Ruff
    
    * Simplify import

--- cuda_pathfinder/cuda/pathfinder/__init__.py
+++ cuda_pathfinder/cuda/pathfinder/__init__.py
@@ -1,10 +1,11 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from cuda.pathfinder._version import __version__  # noqa: F401
+
 from cuda.pathfinder._dynamic_libs.load_dl_common import DynamicLibNotFoundError as DynamicLibNotFoundError
 from cuda.pathfinder._dynamic_libs.load_dl_common import LoadedDL as LoadedDL
 from cuda.pathfinder._dynamic_libs.load_nvidia_dynamic_lib import load_nvidia_dynamic_lib as load_nvidia_dynamic_lib
 from cuda.pathfinder._dynamic_libs.supported_nvidia_libs import (
     SUPPORTED_LIBNAMES as SUPPORTED_NVIDIA_LIBNAMES,  # noqa: F401
 )
-from cuda.pathfinder._version import __version__ as __version__
