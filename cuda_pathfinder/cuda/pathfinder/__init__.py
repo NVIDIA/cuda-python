972d998292ffb773ef4caf4f69ab5e735da9024b
Michael Droettboom <mdboom@gmail.com>
2025-12-19 13:27:08 -0500

    Simplify import

--- cuda_pathfinder/cuda/pathfinder/__init__.py
+++ cuda_pathfinder/cuda/pathfinder/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from cuda.pathfinder._version import __version__ as __version__
+from cuda.pathfinder._version import __version__  # noqa: F401
 
 from cuda.pathfinder._dynamic_libs.load_dl_common import DynamicLibNotFoundError as DynamicLibNotFoundError
 from cuda.pathfinder._dynamic_libs.load_dl_common import LoadedDL as LoadedDL
