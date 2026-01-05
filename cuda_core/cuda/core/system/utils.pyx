e2bc6e53030977dbcbb7ee62db4df324959f330e
Michael Droettboom <mdboom@gmail.com>
2026-01-05 17:05:55 -0500

    Fix exposure of unpack_bitmask

--- cuda_core/cuda/core/system/utils.pyx
+++ cuda_core/cuda/core/system/utils.pyx
@@ -6,7 +6,7 @@ from cpython cimport array
 from libc.stdint cimport uint64_t
 
 
-cdef list[int] unpack_bitmask(x: list[int] | array.array):
+def unpack_bitmask(x: list[int] | array.array) -> list[int]:
     """
     Unpack a list of integers containing bitmasks.
 
