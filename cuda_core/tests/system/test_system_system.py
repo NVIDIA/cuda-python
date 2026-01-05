dcfee0bb8457e8294435be1368fb4c0a7de8ba8c
Michael Droettboom <mdboom@gmail.com>
2026-01-05 15:57:49 -0500

    Move some tests

--- cuda_core/tests/system/test_system_system.py
+++ cuda_core/tests/system/test_system_system.py
@@ -7,9 +7,39 @@
 import os
 
 import pytest
-from cuda.core import system
+
+try:
+    from cuda.bindings import driver, runtime
+except ImportError:
+    from cuda import cuda as driver
+    from cuda import cudart as runtime
+
+from cuda.core import Device, system
+from cuda.core._utils.cuda_utils import handle_return
 
 from .conftest import skip_if_nvml_unsupported
+
+
+def test_driver_version():
+    driver_version = system.get_driver_version()
+    version = handle_return(driver.cuDriverGetVersion())
+    expected_driver_version = (version // 1000, (version % 1000) // 10)
+    assert driver_version == expected_driver_version, "Driver version does not match expected value"
+
+
+def test_num_devices():
+    num_devices = system.get_num_devices()
+    expected_num_devices = handle_return(runtime.cudaGetDeviceCount())
+    assert num_devices == expected_num_devices, "Number of devices does not match expected value"
+
+
+def test_devices():
+    devices = Device.get_all_devices()
+    expected_num_devices = handle_return(runtime.cudaGetDeviceCount())
+    expected_devices = tuple(Device(device_id) for device_id in range(expected_num_devices))
+    assert len(devices) == len(expected_devices), "Number of devices does not match expected value"
+    for device, expected_device in zip(devices, expected_devices):
+        assert device.device_id == expected_device.device_id, "Device ID does not match expected value"
 
 
 def test_cuda_driver_version():
