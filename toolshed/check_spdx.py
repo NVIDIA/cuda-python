4fd633a7fe77c2891964797a7d3ed50d9cac4d65
Michael Droettboom <mdboom@gmail.com>
2026-01-20 11:24:56 -0500

    Update copyright year with a precommit hook

--- toolshed/check_spdx.py
+++ toolshed/check_spdx.py
@@ -1,7 +1,9 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+import datetime
 import os
+import re
 import sys
 
 import pathspec
@@ -26,21 +28,67 @@ def load_spdx_ignore():
     return pathspec.PathSpec.from_lines("gitwildmatch", lines)
 
 
-def has_spdx_or_is_empty(filepath):
+COPYRIGHT_REGEX = (
+    rb"Copyright \(c\) (?P<years>[0-9]{4}(-[0-9]{4})?) "
+    rb"(?P<affiliation>NVIDIA CORPORATION( & AFFILIATES\. All rights reserved\.)?)"
+)
+COPYRIGHT_SUB = r"Copyright (c) {} \g<affiliation>"
+CURRENT_YEAR = str(datetime.datetime.now().year)
+
+
+def find_or_fix_spdx(filepath, fix):
     with open(filepath, "rb") as f:
         blob = f.read()
     if len(blob.strip()) == 0:
         return True
+
     good = True
     for expected_bytes in EXPECTED_SPDX_BYTES:
         if expected_bytes not in blob:
             print(f"MISSING {expected_bytes.decode()}{filepath!r}")
             good = False
+            continue
+
+        match = re.search(COPYRIGHT_REGEX, blob)
+        if match is None:
+            print(f"MISSING valid copyright line in {filepath!r}")
+            good = False
+            continue
+
+        years = match.group("years").decode()
+        if "-" in years:
+            start_year, end_year = years.split("-", 1)
+            if int(start_year) > int(end_year):
+                print(f"INVALID copyright years {years!r} in {filepath!r}")
+                good = False
+        else:
+            start_year = end_year = years
+
+        if int(end_year) < int(CURRENT_YEAR):
+            print(f"OUTDATED copyright end year {years!r} in {filepath!r}")
+            good = False
+
+            if fix:
+                new_years = f"{start_year}-{CURRENT_YEAR}"
+                blob = re.sub(
+                    COPYRIGHT_REGEX,
+                    COPYRIGHT_SUB.format(new_years).encode("ascii"),
+                    blob,
+                )
+                with open(filepath, "wb") as f:
+                    f.write(blob)
+
     return good
 
 
 def main(args):
     assert args, "filepaths expected to be passed from pre-commit"
+
+    if "--fix" in args:
+        fix = True
+        del args[args.index("--fix")]
+    else:
+        fix = False
 
     ignore_spec = load_spdx_ignore()
 
@@ -48,7 +96,7 @@ def main(args):
     for filepath in args:
         if ignore_spec.match_file(filepath):
             continue
-        if not has_spdx_or_is_empty(filepath):
+        if not find_or_fix_spdx(filepath, fix):
             returncode = 1
     return returncode
 
