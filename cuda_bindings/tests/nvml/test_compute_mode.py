ff3759325ac9e8dd51f83f504c83b801d0db27df
Michael Droettboom <mdboom@gmail.com>
2026-01-26 11:30:50 -0500

    Simplify test

--- cuda_bindings/tests/nvml/test_compute_mode.py
+++ cuda_bindings/tests/nvml/test_compute_mode.py
@@ -18,7 +18,6 @@ COMPUTE_MODES = [
 
 @pytest.mark.skipif(sys.platform == "win32", reason="Test not supported on Windows")
 def test_compute_mode_supported_nonroot(all_devices):
-    skipped_devices = {}  # Using dict (not set) to preserve insertion order.
     for device in all_devices:
         with unsupported_before(device, None):
             original_compute_mode = nvml.device_get_compute_mode(device)
@@ -27,14 +26,6 @@ def test_compute_mode_supported_nonroot(all_devices):
             try:
                 nvml.device_set_compute_mode(device, cm)
             except nvml.NoPermissionError:
-                skipped_devices[str(device)] = 1  # value is not used.
-            else:
-                nvml.device_set_compute_mode(device, original_compute_mode)
-                assert original_compute_mode == nvml.device_get_compute_mode(device), (
-                    "Compute mode shouldn't have changed"
-                )
-
-    if skipped_devices:
-        pytest.skip(
-            f"nvmlDeviceSetComputeMode requires root for {len(skipped_devices)} device(s): {', '.join(skipped_devices)}"
-        )
+                pytest.skip("Insufficient permissions to set compute mode")
+            nvml.device_set_compute_mode(device, original_compute_mode)
+            assert original_compute_mode == nvml.device_get_compute_mode(device), "Compute mode shouldn't have changed"
