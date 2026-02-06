463280b28b12fa4c0ed3c8f18515de50c59cade4
Michael Droettboom <mdboom@gmail.com>
2026-02-06 14:30:52 -0500

    Add typing and comments

--- cuda_bindings/cuda/bindings/utils/_fast_enum.py
+++ cuda_bindings/cuda/bindings/utils/_fast_enum.py
@@ -2,25 +2,35 @@
 # SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
 
 
-from typing import Any
+"""
+This is a replacement for the stdlib enum.IntEnum.
+
+Notably, it has much better import time performance, since it doesn't need to
+generate and evaluate Python code.
+
+It supports the most important subset of the IntEnum API.  See `test_enum` in
+`cuda_bindings/tests/test_basics.py` for details.
+"""
+
+from typing import Any, Iterator
 
 
 class _FastEnum(int):
-    def __new__(cls, value):
-        singleton = cls.__singletons__.get(value)
+    def __new__(cls, value: int) -> "_FastEnum":
+        singleton: _FastEnum = cls.__singletons__.get(value)
         if singleton is None:
             raise ValueError(f"{value} is not a valid {cls.__name__}")
         return singleton
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<{self.__class__.__name__}.{self._name}: {int(self)}>"
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @property
-    def value(self):
+    def value(self) -> int:
         return int(self)
 
 
@@ -43,13 +53,13 @@ class _FastEnumMetaclass(type):
         for name, member in cls.__members__.items():
             setattr(cls, name, member)
 
-    def __repr__(cls):
+    def __repr__(cls) -> str:
         return f"<enum '{cls.__name__}'>"
 
-    def __len__(cls):
+    def __len__(cls) -> int:
         return len(cls.__members__)
 
-    def __iter__(cls):
+    def __iter__(cls) -> Iterator[_FastEnum]:
         return iter(cls.__members__.values())
 
     def __contains__(cls, item: Any) -> bool:
