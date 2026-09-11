68a1b19ca5c79f2c83c5ca8eba6c066ea7a10a9a
Michael Droettboom <mdroettboom@nvidia.com>
2026-09-11 10:17:10 -0400

    Regenerate bindings: !539: Fix enum value naming
    Generated from 1b17f396f5854144354b79dc1f4abf94a3d4d6b9 + maint-13.4

--- cuda_bindings/cuda/bindings/cudla.pyx
+++ cuda_bindings/cuda/bindings/cudla.pyx
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # This code was automatically generated across versions from 1.5.0 to 13.4.1. Do not modify it directly.
-# CYTHON-BINDINGS-GENERATED-DO-NOT-MODIFY-THIS-FILE: format=1; content-sha256=9e45316dd031b40a2b5d3cb61e6e0e249d7e27933f5cc7c99e469dddd2efdadc
+# CYTHON-BINDINGS-GENERATED-DO-NOT-MODIFY-THIS-FILE: format=1; content-sha256=03cc93804b4ef509c1f492fcca6b9ec920bf59d91e27b7df9abb6c639bd4dc4c
 
 
 # <<<< PREAMBLE CONTENT >>>>
@@ -1626,23 +1626,30 @@ class FenceType(_cyb_IntEnum):
     """
     See `cudlaFenceType`.
     """
-    NVSCISYNC_FENCE = CUDLA_NVSCISYNC_FENCE
-    NVSCISYNC_FENCE_SOF = CUDLA_NVSCISYNC_FENCE_SOF
+    FENCE = CUDLA_NVSCISYNC_FENCE
+    NVSCISYNC_FENCE = FENCE  # backward-compat alias
+    FENCE_SOF = CUDLA_NVSCISYNC_FENCE_SOF
+    NVSCISYNC_FENCE_SOF = FENCE_SOF  # backward-compat alias
 
 class ModuleLoadFlags(_cyb_IntEnum):
     """
     See `cudlaModuleLoadFlags`.
     """
-    MODULE_DEFAULT = CUDLA_MODULE_DEFAULT
-    MODULE_ENABLE_FAULT_DIAGNOSTICS = CUDLA_MODULE_ENABLE_FAULT_DIAGNOSTICS
+    DEFAULT = CUDLA_MODULE_DEFAULT
+    MODULE_DEFAULT = DEFAULT  # backward-compat alias
+    ENABLE_FAULT_DIAGNOSTICS = CUDLA_MODULE_ENABLE_FAULT_DIAGNOSTICS
+    MODULE_ENABLE_FAULT_DIAGNOSTICS = ENABLE_FAULT_DIAGNOSTICS  # backward-compat alias
 
 class SubmissionFlags(_cyb_IntEnum):
     """
     See `cudlaSubmissionFlags`.
     """
-    SUBMIT_NOOP = CUDLA_SUBMIT_NOOP
-    SUBMIT_SKIP_LOCK_ACQUIRE = CUDLA_SUBMIT_SKIP_LOCK_ACQUIRE
-    SUBMIT_DIAGNOSTICS_TASK = CUDLA_SUBMIT_DIAGNOSTICS_TASK
+    NOOP = CUDLA_SUBMIT_NOOP
+    SUBMIT_NOOP = NOOP  # backward-compat alias
+    SKIP_LOCK_ACQUIRE = CUDLA_SUBMIT_SKIP_LOCK_ACQUIRE
+    SUBMIT_SKIP_LOCK_ACQUIRE = SKIP_LOCK_ACQUIRE  # backward-compat alias
+    DIAGNOSTICS_TASK = CUDLA_SUBMIT_DIAGNOSTICS_TASK
+    SUBMIT_DIAGNOSTICS_TASK = DIAGNOSTICS_TASK  # backward-compat alias
 
 class AccessPermissionFlags(_cyb_IntEnum):
     """
@@ -1663,9 +1670,12 @@ class ScratchMemoryConfig(_cyb_IntEnum):
     """
     See `cudlaScratchMemoryConfig`.
     """
-    SCRATCH_MEMORY_DEFAULT = CUDLA_SCRATCH_MEMORY_DEFAULT
-    SCRATCH_MEMORY_SHARED_STATIC = CUDLA_SCRATCH_MEMORY_SHARED_STATIC
-    MAX = CUDLA_SCRATCH_MEMORY_CONFIG_MAX
+    DEFAULT = CUDLA_SCRATCH_MEMORY_DEFAULT
+    SCRATCH_MEMORY_DEFAULT = DEFAULT  # backward-compat alias
+    SHARED_STATIC = CUDLA_SCRATCH_MEMORY_SHARED_STATIC
+    SCRATCH_MEMORY_SHARED_STATIC = SHARED_STATIC  # backward-compat alias
+    CONFIG_MAX = CUDLA_SCRATCH_MEMORY_CONFIG_MAX
+    MAX = CONFIG_MAX  # backward-compat alias
 
 
 ###############################################################################
