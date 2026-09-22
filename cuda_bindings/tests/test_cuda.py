4c3a60472fe1b12f69d33a633c73ef81870bd824
Keith Kraus <keith.j.kraus@gmail.com>
2026-09-22 00:43:05 -0400

    Format CUDA attribute regression test

--- cuda_bindings/tests/test_cuda.py
+++ cuda_bindings/tests/test_cuda.py
@@ -559,9 +559,7 @@ def test_cuda_coredump_attr():
 
 @pytest.mark.agent_authored(model="gpt-5.6-sol")
 def test_coredump_bool_uses_single_byte_storage():
-    helper = cuda._HelperCUcoredumpSettings(
-        cuda.CUcoredumpSettings.CU_COREDUMP_TRIGGER_HOST, 0, is_getter=True
-    )
+    helper = cuda._HelperCUcoredumpSettings(cuda.CUcoredumpSettings.CU_COREDUMP_TRIGGER_HOST, 0, is_getter=True)
     ctypes.c_uint32.from_address(helper.cptr).value = 0xFFFFFFFF
     ctypes.c_uint8.from_address(helper.cptr).value = 0
 
